c5d1bdf2a3e9bdda28955123bcc8155a3318a86c
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-10-23 20:07:21 +0600

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,3 +17,9 @@ export default function RootLayout({
     </html>
   );
 }
+
+// <reference types="next" />
+// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
